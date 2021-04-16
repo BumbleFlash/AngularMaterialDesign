8612f3b235a3f374bd968d514bd82ca49744a638
BumbleFlash <sudarshan97@gmail.com>
2021-04-16 16:39:58 -0400

    feat: added checkbox events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MaterialDesign';
+  isChecked = true;
+
+  onChange($event): void {
+    console.log($event);
+  }
 }
